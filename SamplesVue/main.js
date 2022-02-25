47238ab2d6309a075aa94fdd5a5d9592ace811a0
Juan Salcedo Almonte <jsalcedo@namadr.com>
2022-02-25 16:33:27 -0500

    added VUE Sample
    added VUE Sample

---
+++ SamplesVue/main.js
@@ -0,0 +1,17 @@
+const app = new Vue({
+    /*connecta el vue al DOM */
+    el: '#App',
+    data: {
+        product: 'socks',
+        image:'IMG/green.jpg',
+        inventory: 5,
+        onSale: true
+    }
+    
+
+});
+
+
+/* var socks = 'blackSocks';
+
+document.getElementById('App').innerHTML = socks; */
